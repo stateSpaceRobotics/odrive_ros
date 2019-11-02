adb3016e47af33ca5fa944941ababf463ed67f55
Lucas D. Cagle <ldc290@msstate.edu>
2019-11-02 17:05:55 -0500

    Added comment on how to enable debug mode

--- scripts/odrive_node.py
+++ scripts/odrive_node.py
@@ -3,7 +3,7 @@ import rospy
 from odrive_ros.odrive_interface import ODrive_ROS
 import pprint
 if __name__=="__main__":
-    rospy.init_node("odrive_node")
+    rospy.init_node("odrive_node")#, log_level=rospy.DEBUG)
     odrive_node = ODrive_ROS("odrive_node")
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
