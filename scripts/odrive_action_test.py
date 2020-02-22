4f60fbcd6db08f7f7d71665d1cd7d04b0efa12e4
Lucas D. Cagle <daftpwner@gmail.com>
2020-02-21 22:49:12 -0600

    Example action didn't showcase tolerance setting.

--- scripts/odrive_action_test.py
+++ scripts/odrive_action_test.py
@@ -12,7 +12,7 @@ if __name__=="__main__":
     rospy.init_node("test_odrive_action")
     client = actionlib.SimpleActionClient("/name0/velocity",odrive_ros.msg.SetpointAction)
     client.wait_for_server()
-    goal = odrive_ros.msg.SetpointGoal(setpoint=0)
+    goal = odrive_ros.msg.SetpointGoal(setpoint=0, tolerance=0.25)
     client.send_goal(goal,feedback_cb=fdbk_cb)
     client.wait_for_result()
     print(client.get_result())
